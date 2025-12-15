a64ba24098234d7028940c34bbf0e4b3363948a4
Mr5niper <davidx.l.johnson@gmail.com>
2025-12-14 19:27:58 -0800

    Update arima.py

--- stock_forecast/models/arima.py
+++ stock_forecast/models/arima.py
@@ -1,14 +1,16 @@
 # stock_forecast/models/arima.py
 import numpy as np
 from statsmodels.tsa.arima.model import ARIMA
-
 def arima_forecast(train_series, order=(3,1,2)):
     import warnings
     warnings.filterwarnings("ignore")
     try:
-        model = ARIMA(train_series, order=order)
+        arr = np.asarray(train_series, dtype=float).ravel()
+        if len(arr) < sum(order) + 3:
+            return float(arr[-1])
+        model = ARIMA(arr, order=order)
         fitted = model.fit()
         yhat = fitted.forecast(steps=1)
         return float(np.asarray(yhat)[0])
     except Exception:
-        return float(train_series[-1])
+        return float(np.asarray(train_series).ravel()[-1])
