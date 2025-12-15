b3059a9e8caf67baadc1170e652f6675c5237796
Mr5niper <davidx.l.johnson@gmail.com>
2025-12-14 18:42:07 -0800

    Update __init__.py

--- stock_forecast/__init__.py
+++ stock_forecast/__init__.py
@@ -1,6 +1,7 @@
 # stock_forecast/__init__.py
 __all__ = [
     "utils", "dataset", "splits", "metrics",
-    "models", "train_lstm", "train_gan", "eval", "backtest", "meta_labeling"
+    "models", "train_lstm", "train_gan", "eval",
+    "backtest", "meta_labeling", "gann_grid"
 ]
 __version__ = "4.0.0"
